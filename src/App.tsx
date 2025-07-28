250399e091b722f32f28439047ad1464bdfc7c61
rramires <ricardo@flexbr.com>
2025-07-28 10:40:15 -0300

    Adding normal styling with css example.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
+import { Button } from "./components/Button";
+
 export function App() {
   
   return (
-    <h1>Hello World!!!</h1>
+    <>
+      <h1>Hello World!!!</h1>
+
+      <Button />
+      <Button color="primary"/>
+      <Button color="secondary"/>
+      <Button color="danger"/>
+      <Button color="success"/>
+    </>
   )
 }
