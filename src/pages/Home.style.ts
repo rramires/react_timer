106ebe3da1a47f21bbcbac0087d4a8450661c88c
rramires <ricardo@flexbr.com>
2025-08-09 10:35:40 -0300

    Adding and styling home.

---
+++ src/pages/Home.style.ts
@@ -0,0 +1,119 @@
+import styled from 'styled-components'
+
+export const HomeContainer = styled.main`
+	flex: 1;
+
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+
+	& form {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 3.5rem;
+	}
+`
+
+export const FormContainer = styled.main`
+	width: 100%;
+
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	gap: 0.5rem;
+	flex-wrap: wrap;
+
+	color: ${(props) => props.theme['gray-100']};
+	font-size: 1.125rem;
+	font-weight: bold;
+`
+
+const BaseInput = styled.input`
+	height: 2.5rem;
+	padding: 0 0.5rem;
+
+	background: transparent;
+	border: 0;
+	border-bottom: 2px solid ${(props) => props.theme['gray-500']};
+
+	font-size: inherit;
+	font-weight: inherit;
+	color: inherit;
+
+	&::placeholder {
+		color: ${(props) => props.theme['gray-500']};
+	}
+
+	&:focus {
+		box-shadow: none;
+		border-color: ${(props) => props.theme['green-500']};
+	}
+`
+
+export const TaskInput = styled(BaseInput)`
+	flex: 1;
+
+	/* &::-webkit-calendar-picker-indicator {
+		display: none !important;
+	} */
+`
+
+export const TimeInput = styled(BaseInput)`
+	width: 4rem;
+`
+
+export const CountdownContainer = styled.main`
+	display: flex;
+	gap: 1rem;
+
+	font-family: 'Roboto Mono';
+	font-size: 10rem;
+	line-height: 8rem;
+
+	color: ${(props) => props.theme['gray-100']};
+
+	& span {
+		padding: 2rem 1rem;
+		border-radius: 8px;
+		background: ${(props) => props.theme['gray-700']};
+	}
+
+	& div {
+		width: 4rem;
+		padding: 2rem 0;
+
+		display: flex;
+		align-items: end;
+		justify-content: center;
+
+		color: ${(props) => props.theme['green-500']};
+	}
+`
+
+export const StartButton = styled.button`
+	width: 100%;
+	border: none;
+	border-radius: 8px;
+
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	padding: 1rem;
+	gap: 0.5rem;
+
+	font-weight: bold;
+	color: ${(props) => props.theme['gray-100']};
+	background: ${(props) => props.theme['green-500']};
+	cursor: pointer;
+
+	&:disabled {
+		opacity: 0.7;
+		cursor: not-allowed;
+	}
+
+	&:not(:disabled):hover {
+		background: ${(props) => props.theme['green-700']};
+	}
+`
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,54 @@
+import { Play } from 'phosphor-react'
+import {
+	HomeContainer,
+	FormContainer,
+	CountdownContainer,
+	StartButton,
+	TaskInput,
+	TimeInput,
+} from './Home.style'
+
 export function Home() {
 	return (
-		<div>
-			<h1>Home</h1>
-		</div>
+		<HomeContainer>
+			<form action=''>
+				<FormContainer>
+					<label htmlFor='task'>Vou trabalhar em</label>
+					<TaskInput
+						id='task'
+						type='text'
+						placeholder='Dê um nome para sua tarefa'
+						list='task-suggestions'
+					/>
+					<datalist id='task-suggestions'>
+						<option value='Tarefa 1' />
+						<option value='Tarefa 2' />
+						<option value='Tarefa 3' />
+						<option value='Outra sujestão' />
+					</datalist>
+					<label htmlFor='minutes'>Durante</label>
+					<TimeInput
+						id='minutes'
+						type='number'
+						step={5}
+						min={5}
+						max={60}
+						placeholder='00'
+					/>
+					<span>minutos.</span>
+				</FormContainer>
+				<CountdownContainer>
+					<span>0</span>
+					<span>0</span>
+					<div>:</div>
+					<span>0</span>
+					<span>0</span>
+				</CountdownContainer>
+				<StartButton type='submit'>
+					Começar
+					<Play size={24} />
+				</StartButton>
+			</form>
+		</HomeContainer>
 	)
 }
